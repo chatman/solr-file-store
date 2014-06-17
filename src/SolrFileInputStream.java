777fa575a5819cdab0647f06998d6d27ac1cd8ab
Ishan Chattopadhyaya <ichattopadhyaya@gmail.com>
2014-06-17 19:41:08 +0530

    Adding indexing based on meta document

--- src/SolrFileInputStream.java
+++ src/SolrFileInputStream.java
@@ -1,6 +1,8 @@
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.solr.client.solrj.SolrServer;
 import org.apache.solr.client.solrj.SolrServerException;
@@ -25,13 +27,42 @@ public class SolrFileInputStream extends InputStream {
   
   int start = 0;
   long size = 0;
+  List<String> parts; 
   
-  private ByteBuffer fetch() throws SolrServerException {
+  void fetchMeta() throws SolrServerException {
     ModifiableSolrParams query = new ModifiableSolrParams();
     query.set("q", "name:"+filename);
-    query.set("rows", "10");
-    query.set("sort", "part asc");
-    query.set("start", start);
+    query.set("rows", "1");
+    
+    QueryResponse resp = server.query(query);
+    SolrDocumentList res = resp.getResults();
+
+    if(res.size()==0) {
+      return;
+    } else {
+      for (SolrDocument d : res) {
+        parts = new ArrayList<>();
+        for(Object v: d.getFieldValues("parts")) {
+          parts.add(v.toString());
+        }
+        break;
+      }
+    }
+  }
+  
+  final int BATCH_SIZE = 10;
+  
+  private ByteBuffer fetch() throws SolrServerException {
+    if(parts==null || parts.size()==0 || start>=parts.size())
+      return null;
+    
+    ModifiableSolrParams query = new ModifiableSolrParams();
+    StringBuffer queryStr = new StringBuffer("id:"+parts.get(start));
+    for(int i=start+1; i<start+BATCH_SIZE && i<parts.size(); i++)
+      queryStr.append(" OR id:"+parts.get(i));
+    
+    query.set("q", queryStr.toString());
+    query.set("sort", "sequence asc");
 
     QueryResponse resp = server.query(query);
     SolrDocumentList res = resp.getResults();
@@ -70,6 +101,15 @@ public class SolrFileInputStream extends InputStream {
 
   @Override
   public int read() throws IOException {
+    if(parts==null) {
+      try {
+        fetchMeta();
+      } catch (SolrServerException e) {
+        e.printStackTrace();
+      }
+      if(parts==null)
+        return -1;
+    }
     if(buffer==null || buffer.limit()==position) {
       try {
         if(fetch()==null)
--- src/SolrFileStore.java
+++ src/SolrFileStore.java
@@ -5,7 +5,13 @@ import java.io.InputStream;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.solr.client.solrj.SolrServer;
 import org.apache.solr.client.solrj.SolrServerException;
@@ -17,57 +23,88 @@ public class SolrFileStore {
 
   final static int BUFFER_SIZE = 1024*1024*15;
 
-  static int streamToSolr(String name, String path, SolrServer server) throws IOException, SolrServerException {
+  static int streamToSolr(String name, String path, SolrServer server) throws IOException, SolrServerException, NoSuchAlgorithmException {
     RandomAccessFile aFile = new RandomAccessFile(path, "r");
     FileChannel inChannel = aFile.getChannel();
     ByteBuffer buffer = ByteBuffer.allocate(BUFFER_SIZE);
 
     int counter = 0;
+    long size = 0;
+
+    String uuid = UUID.randomUUID().toString();
     
-    server.deleteByQuery("name:"+name);
-    server.commit();
+    List<String> ids = new ArrayList<>();
     
     while(inChannel.read(buffer) > 0)
     {
       buffer.flip();
       counter++;
-      SolrInputDocument doc = new SolrInputDocument();
-      doc.addField("id", name+"_"+counter);
-      doc.addField("name", name);
-      doc.addField("rawdata", ByteBuffer.wrap(buffer.array().clone(), 0, buffer.remaining()));
-      doc.addField("part", counter);
-      System.out.println(name+"_"+counter);
+      SolrInputDocument partDoc = new SolrInputDocument();
+      
+      partDoc.addField("rawdata", ByteBuffer.wrap(buffer.array().clone(), 0, buffer.remaining()));
+      partDoc.addField("sequence", counter);
+      
+      size+=buffer.remaining();
+      
+      // Generate md5 for part
+      /*StringBuffer md5 = new StringBuffer();
+      try {
+        java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
+        byte[] array = md.digest((new String(buffer.array())).getBytes("UTF-8"));
+        for (int i = 0; i < array.length; ++i) {
+          md5.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1,3));
+        }
+      } catch (java.security.NoSuchAlgorithmException e) {
+        
+      }*/
+      
+      String partId = uuid+"_"+counter;
+      partDoc.addField("id", partId);
+      ids.add(partId);
+
       if(counter%2==0) {
         server.commit();
-        System.out.println("Committing..");
+        System.out.println("Committing.. "+"Parts written: "+counter);
       }
-      server.add(doc);
+      server.add(partDoc);
       buffer.clear();
     }
-    
+
     inChannel.close();
     aFile.close();
     server.commit();
-    System.out.println("Committed...");
-
+    System.out.println("Committed "+counter+" parts...");
+    
+    
+    server.deleteByQuery("name:"+name);
+    SolrInputDocument metaDoc = new SolrInputDocument();
+    metaDoc.addField("id", uuid);
+    metaDoc.addField("name", name);
+    for(String p: ids)
+      metaDoc.addField("parts", p);
+    metaDoc.addField("size", size); 
+    server.add(metaDoc);
+    server.commit();
+    System.out.println("Committed meta document: "+metaDoc);
+    
     return counter;
   }
 
-  public static void main(String[] args) throws SolrServerException, IOException {
+  public static void main(String[] args) throws SolrServerException, IOException, NoSuchAlgorithmException {
     SolrServer server = new HttpSolrServer("http://localhost:8983/solr");
 
     long startTime = System.currentTimeMillis();
-    streamToSolr("en-pos-maxent.bin", "en-pos-maxent.bin", server);
+    streamToSolr("xp.iso", "xp.iso", server);
     long endTime = System.currentTimeMillis();
 
     System.out.println("Indexing time: "+(endTime-startTime));
 
     startTime = System.currentTimeMillis();
     FileOutputStream fos = new FileOutputStream(new File("test.out"));
-    InputStream is = new SolrFileInputStream(server, "en-pos-maxent.bin");
+    InputStream is = new SolrFileInputStream(server, "xp.iso");
 
     IOUtils.copy(is, fos);
-    
+
     //is.close();
     fos.close();
     endTime = System.currentTimeMillis();
